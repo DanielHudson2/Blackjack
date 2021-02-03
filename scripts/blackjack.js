c2b5c0f17cabfd031b340eb6f045234716f716fb
Daniel Hudson <35522603+DanielHudson2@users.noreply.github.com>
2021-02-03 23:19:15 +0000

    new: added card variables and create deck function

---
+++ scripts/blackjack.js
@@ -0,0 +1,17 @@
+// card variables
+let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
+
+function createDeck() {
+    let deck = [];
+    for (let suitIndex = 0; suitIndex < suits.length; suitIndex++) {
+        for (let valueIndex = 0; valueIndex < values.length; valueIndex++) {
+            let card = {
+                suit: suits[suitIndex],
+                value: values[valueIndex]
+            };
+            deck.push(card);
+        }
+    }
+    return deck;
+}
